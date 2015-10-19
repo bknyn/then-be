9c3cd625de19af42d7678d6c39ba4c6bf35e9b1c
Bradley Kenyon <b@bradleykenyon.com>
2015-10-19 14:52:25 -0700

    basic logic flo

--- source/js/then-be.js
+++ source/js/then-be.js
@@ -4,16 +4,24 @@
 
     _bindEvents: function() {
       var self = this;
+
+      this.elements.panelChangeLink.on('click', function(e) {
+        e.preventDefault();
+        var target = $(this).data('target');
+        self.elements.body.data('state', target).attr('data-state', target);
+      });
     },
 
     _getElements: function() {
       this.elements.window = $(window);
+      this.elements.body = $('body');
+
+      this.elements.panelChangeLink = $('a.panel-change');
     },
 
     initialize: function() {
       this._getElements();
       this._bindEvents();
-      console.log('foo');
     }
   };
 
